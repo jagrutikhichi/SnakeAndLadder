d88d18eba42d10c32d464fa0f8be404d8049b1b1
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-06-22 20:54:53 +0530

    Added: players position from zero

--- src/com/bridgelab/SnakeandLader.java
+++ src/com/bridgelab/SnakeandLader.java
@@ -5,4 +5,12 @@ import java.util.Queue;
 
 public class SnakeandLader {
 
+	public static void main(String[] args) {
+		
+		int N = 100;
+		int moves[] = new int[N];
+		for (int i = 0; i < N; i++)
+			moves[i] = -1;
+		
+	}
 }
